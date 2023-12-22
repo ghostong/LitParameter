cc073cccb2c4c4efcaafa07687b15d4efd136d4b
litong <ghostong@126.com>
2023-12-22 16:00:21 +0800

    增加错误, 方式读取变量出错

--- src/V2/Types/Types.php
+++ src/V2/Types/Types.php
@@ -102,4 +102,25 @@ class Types
         return $this->getObject()->getType();
     }
 
+    /**
+     * 禁止直接 toString
+     * @date 2023/12/22
+     * @return void
+     * @author litong
+     */
+    public function __toString() {
+        $trace = debug_backtrace(0);
+        trigger_error("Use the value() method to read the variable value. in {$trace[0]["file"]} on line {$trace[0]["line"]}  -> ", E_USER_ERROR);
+    }
+
+    /**
+     * 禁止直接 debugInfo
+     * @date 2023/12/22
+     * @return void
+     * @author litong
+     */
+    public function __debugInfo() {
+        $trace = debug_backtrace(1);
+        trigger_error("Use the value() method to read the variable value. in {$trace[1]["file"]} on line {$trace[1]["line"]}  -> ", E_USER_ERROR);
+    }
 }
