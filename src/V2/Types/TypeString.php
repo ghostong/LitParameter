32770944a03373fc08ef1246071d050a316309e8
litong <ghostong@126.com>
2026-06-15 10:18:55 +0800

    1

--- src/V2/Types/TypeString.php
+++ src/V2/Types/TypeString.php
@@ -8,7 +8,8 @@ class TypeString extends Common
 {
     protected $valueType = "string";
 
-    public function __construct($name) {
+    public function __construct($name)
+    {
         $this->name = $name;
         $this->setWorkShop($this->valueType, function ($value) {
             return is_string($value);
@@ -23,7 +24,8 @@ class TypeString extends Common
      * @return TypeString
      * @author litong
      */
-    public function minLength($length) {
+    public function minLength($length)
+    {
         $this->setWorkShop(__FUNCTION__, function ($value) use ($length) {
             return !(strlen($value) < $length);
         });
@@ -37,7 +39,8 @@ class TypeString extends Common
      * @return TypeString
      * @author litong
      */
-    public function maxLength($length) {
+    public function maxLength($length)
+    {
         $this->setWorkShop(__FUNCTION__, function ($value) use ($length) {
             return !(strlen($value) > $length);
         });
@@ -51,11 +54,56 @@ class TypeString extends Common
      * @return TypeString
      * @author litong
      */
-    public function length($length) {
+    public function length($length)
+    {
         $this->setWorkShop(__FUNCTION__, function ($value) use ($length) {
             return strlen($value) === $length;
         });
         return $this;
     }
+
+
+    /**
+     * 邮箱格式
+     * @date 2026/6/15
+     * @return TypeString
+     * @author litong
+     */
+    public function emailFormat()
+    {
+        $this->setWorkShop(__FUNCTION__, function ($value) {
+            return filter_var($value, FILTER_VALIDATE_EMAIL);
+        });
+        return $this;
+    }
+
+    /**
+     * ipv4格式
+     * @date 2026/6/15
+     * @return TypeString
+     * @author litong
+     */
+    public function ipV4Format()
+    {
+        $this->setWorkShop(__FUNCTION__, function ($value) {
+            return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+        });
+        return $this;
+    }
+
+    /**
+     * ipv6格式
+     * @date 2026/6/15
+     * @return TypeString
+     * @author litong
+     */
+    public function ipV6Format()
+    {
+        $this->setWorkShop(__FUNCTION__, function ($value) {
+            return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+        });
+        return $this;
+    }
+
 
 }
